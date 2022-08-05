212125b1499c76d8b419bca3c4f427a3eb8f467e
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2022-08-05 20:43:54 +0100

    add save, get, delete and put method for adding comments and likes

--- server/comment/src/validation/index.js
+++ server/comment/src/validation/index.js
@@ -4,8 +4,8 @@ const commentValidation = (data) => {
     const schemaValidation = joi.object({
         object_id:joi.string().required().min(20).max(256),
         message:joi.string().required().min(2).max(256),
-        profile_data:joi.array(),
-        liked_data:joi.arra()        
+        created_by:joi.object(),
+        liked_by:joi.array()        
     })
     return schemaValidation.validate(data)
 }
