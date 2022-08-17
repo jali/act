62b686c334bc443373c1ac755fed8b18baaf9d47
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2022-08-17 23:22:00 +0100

    add footer to layout

--- client/src/components/layout/index.jsx
+++ client/src/components/layout/index.jsx
@@ -1,27 +1,37 @@
-
 import React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Header from './header';
+import Footer from './footer';
+import useAuth from 'features/auth/use';
+import useProfile from 'features/profile/use';
+import LoadingPage from 'components/loading-page';
 
 export default function Layout({children}) {
-    return (
-    <>  
-        <Header />
-        <main>
-            {/* Hero unit */}
-            <Box
-              sx={{
-                bgcolor: 'background.paper',
-                pt: 8,
-                pb: 6,
-              }}
-            >
-              <Container maxWidth="sm">
-                {children}
-              </Container>
-            </Box>
-        </main>
-    </>
+  const { loading } = useAuth();
+  const { loading: loadingProfile } = useProfile();
+    return ( 
+        (loading || loadingProfile) ? (
+            <LoadingPage/>
+        ) : (
+            <>
+            <Header />
+            <main>
+                {/* Hero unit */}
+                <Box
+                sx={{
+                    bgcolor: 'background.paper',
+                    pt: 8,
+                    pb: 6,
+                }}
+                >
+                <Container maxWidth="sm">
+                    {children}
+                </Container>
+                </Box>
+            </main>
+            <Footer sx={{ mt: 8, mb: 4 }} />
+            </>
+        )
     );
 };
